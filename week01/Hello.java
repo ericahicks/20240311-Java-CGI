5993f5cdbe42edb8c1bac3ab6dba0ae0791b5f4a
ericahicks <48265629+ericahicks@users.noreply.github.com>
2024-03-11 14:29:00 -0400

    hello world program

---
+++ week01/Hello.java
@@ -0,0 +1,15 @@
+// This is comment the compiler will not read
+/*
+ * This is a multiline comment
+ * for taking notes in your code file
+ * and telling teh JVM to ignore this text
+ */
+public class Hello { // make sure the class name is the same as the file name
+
+    // Put all your code in functions aka "methods"
+    // named blocks of code
+    public static void main(String[] args) {
+        System.out.print("Hello wold!");
+    }
+
+}
