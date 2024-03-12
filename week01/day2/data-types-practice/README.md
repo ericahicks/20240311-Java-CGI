60cb7dced280816502d0541012a3a1095d9e65bc
ericahicks <48265629+ericahicks@users.noreply.github.com>
2024-03-12 15:40:54 -0400

    scanner example

--- week01/day2/data-types-practice/README.md
+++ week01/day2/data-types-practice/README.md
@@ -7,3 +7,4 @@ Today we created a Java Project using Visual Studio Code. Then we created packag
 - CTRL-SHIFT-P opens the command pallet (search 'java' to find the option for creating a project)
 - CTRL-/ comments out a line of code
 - SHIFT-ALT-DOWN-ARROW copies the current line and inserts it in a newline below
+- CTRL-C in the terminal kills the program
--- week01/day2/data-types-practice/src/com/skillstorm/examples/userinput/ScannerExamples.java
+++ week01/day2/data-types-practice/src/com/skillstorm/examples/userinput/ScannerExamples.java
@@ -12,18 +12,39 @@ public class ScannerExamples {
         // Read in a line
         System.out.print("Enter your name: ");
         String name = in.nextLine();
+        System.out.println("Hello, " + name);
 
         // Example 2:
         // Read in the next value
-
-        // Example 3
-        // Read in a number
-
-        // Example 4
-        // Read in a decimal number
+        
+        // in.useDelimiter(",");
+        System.out.print("Enter your city and state separated by a space:  ");
+        String city = in.next(); // Grabs the next token (by default this is everything up to the first whitespace)
+        String state = in.next();
+        System.out.println("You live in " + city + ", " + state);
 
         // Example 5
         // Change the delimiter and read in a value
+        // in.useDelimiter(",");
+
+        // Example 3
+        // Read in a number
+        System.out.print("Enter your zipcode: ");
+        
+        // Debugging:
+        // String nextToken = in.next();
+        // System.out.println("The next token was '" + nextToken + "'");
+
+        int zipcode = in.nextInt();
+        System.out.println("Thank you, you entered a zipcode of " + zipcode);
+
+        // Example 4
+        // Read in a decimal number
+        System.out.println("Enter your weight in pounds: ");
+        double weight = in.nextDouble();
+        System.out.println("Thank you, you entered a weight of " + weight + "lbs");
+
+        
 
         in.close(); // close the connection
         
